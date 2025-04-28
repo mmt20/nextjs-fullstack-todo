de58d65c123494afa0136bd997f350beb5ea73d0
mmt20 <mmt202002@gmail.com>
2025-04-28 06:45:31 +0300

    feat: implement TodoTable component and integrate it into the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,19 @@
 import { getTodoListAction } from "@/actions/todo.actions";
 import AddTodoForm from "@/components/AddTodoForm";
+import TodoTable from "@/components/TodoTable";
 
-export default function Home() {
-  //const todos = await getTodoListAction();
+export default async function Home() {
+  const todos = await getTodoListAction();
 
   return (
     <main className="container flex flex-col  items-center p-24">
       <AddTodoForm />
-      {/* {todos.map(todo => (
-        <div key={todo.id} className="flex items-center justify-between p-4 border-b w-5xl">
-          <div>
-            <h2 className="text-lg font-semibold">{todo.title}</h2>
-            <p>{todo.body}</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <Button variant="outline" size="sm">
-              Edit
-            </Button>
-            <Button variant="destructive" size="sm">
-              Delete
-            </Button>
-          </div>
-        </div>
-      ))} */}
+      <TodoTable />
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </main>
   );
 }
