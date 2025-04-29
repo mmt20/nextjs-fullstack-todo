480c2a85885ed697bfc1b89e30bb6135cddd9bad
mmt20 <mmt202002@gmail.com>
2025-04-29 15:59:27 +0300

    refactor: comment out fake data generation for user model in seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,19 +3,6 @@ import { PrismaClient } from "./app/generated/prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // ** Genrate fake dat for user model **
-  // await prisma.user.createMany({
-  //   data: Array.from({ length: 10 }, () => ({
-  //     email: faker.internet.email(),
-  //     name: faker.internet.username(),
-  //     address: {
-  //       street: faker.location.street(),
-  //       city: faker.location.city(),
-  //       state: faker.location.state(),
-  //       zip: faker.location.zipCode(),
-  //     },
-  //   })),
-  // });
   // ** Genrate fake dat for todo model **
   // await prisma.todo.createMany({
   //   data: Array.from({ length: 10 }, () => ({
